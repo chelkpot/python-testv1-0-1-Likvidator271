cb635c4deed79d220847d31fa49bd9836b4686dc
student <student@corp.kpiot.ru>
2025-09-22 16:56:51 +0500

    ZZ

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,8 +2,13 @@
 
 
 
-def solve():
+def solve():   
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
+    chislo1, chislo2, chislo3 = map(int, input().split())
+    a = (chislo1 ** 2)
+    b = (chislo2 ** 2)
+    c = (chislo3 ** 2)
+    print(a,b,c)
 
 
 # Код ниже не трогать! он нужен для тестов
